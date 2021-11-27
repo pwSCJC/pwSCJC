2ae2cae22b4361dea17d3bbe5e9fc05451b91b5f
pwSCJC <95137090+pwSCJC@users.noreply.github.com>
2021-11-27 13:16:22 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -7,12 +7,13 @@
  * quedar igual que en tu
  * servidor */
 // @ts-ignore
-const firebaseConfig = {
-  apiKey: "AIzaSyD0lYqXG_I8Zp5ih83euoLVrXEZd66-zlc",
+firebase.initializeApp({
+ apiKey: "AIzaSyD0lYqXG_I8Zp5ih83euoLVrXEZd66-zlc",
   authDomain: "pwscjc-e87db.firebaseapp.com",
   projectId: "pwscjc-e87db",
   storageBucket: "pwscjc-e87db.appspot.com",
   messagingSenderId: "458818571022",
   appId: "1:458818571022:web:8519ab38fa44845803e55f",
   measurementId: "G-BYCT0WQYZM"
-};
+});
+
